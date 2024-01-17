4ce3fcf879ffe637da119864284701750bdcb51a
Milind Dhamu <milind.dhamu.111@gmail.com>
2024-01-17 11:07:26 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,26 +1,78 @@
 // create web server
-// create a path to public
-const publicPath = path.resolve(__dirname, 'public');
-// create a path to views
-const viewsPath = path.resolve(__dirname, 'views');
-// create a path to partials
-const partialsPath = path.resolve(__dirname, 'views/partials');
-// create a path to handlebars
-const handlebars = require('express-handlebars');
-// create a path to body-parser
-const bodyParser = require('body-parser');
-// create a path to mongoose
-const mongoose = require('mongoose');
-// create a path to method-override
-const methodOverride = require('method-override');
-// create a path to connect-flash
-const flash = require('connect-flash');
-// create a path to express-session
-const session = require('express-session');
-// create a path to passport
-const passport = require('passport');
-// create a path to passport-local
-const localStrategy = require('passport-local');
-// create a path to express-validator
-const expressValidator = require('express-validator');
-// create a path to moment
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var path = require('path');
+var commentsPath = path.join(__dirname, 'comments.json');
+var comments = JSON.parse(fs.readFileSync(commentsPath, 'utf8'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/api/comments', function(req, res) {
+  res.setHeader('Cache-Control', 'no-cache');
+  res.json(comments);
+});
+
+app.post('/api/comments', function(req, res) {
+  comments.push(req.body);
+  fs.writeFile(commentsPath, JSON.stringify(comments, null, 4), function(err) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    res.setHeader('Cache-Control', 'no-cache');
+    res.json(comments);
+  });
+});
+
+app.listen(3000, function() {
+  console.log('Server started: http://localhost:3000/');
+});
+```
+
+### 3.4.2. Using the Comment component
+
+```javascript
+// Path: CommentBox.js
+var CommentBox = React.createClass({
+  loadCommentsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(comments) {
+        this.setState({data: comments});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  handleCommentSubmit: function(comment) {
+    var comments = this.state.data;
+    comment.id = Date.now();
+    var newComments = comments.concat([comment]);
+    this.setState({data: newComments});
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: comment,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        this.setState({data: comments});
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
